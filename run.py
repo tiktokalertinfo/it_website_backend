2a5505fbddc0bcb7dc30de770955a5fc64491600
tiktokalertinfo <H2SO4.1191@gmail.com>
2025-08-28 14:06:40 +0300

    Run again

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+# run.py
 import os
 from django.core.management import execute_from_command_line
 
-port = os.environ.get("PORT", "8000")
-execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    port = os.environ.get("PORT", "8000")
+    execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
